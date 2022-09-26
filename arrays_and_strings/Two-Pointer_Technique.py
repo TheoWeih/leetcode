4a478a273affa2ee3a864f72be384eee095c8d95
Theo Weihprachtitzky <Theo.Weihprachtitzky@ibm.com>
2022-09-26 14:35:15 +0200

    Added twoSum

--- arrays_and_strings/Two-Pointer_Technique.py
+++ arrays_and_strings/Two-Pointer_Technique.py
@@ -45,3 +45,35 @@ class Solution:
             result += a
 
         return result
+
+# https://leetcode.com/explore/learn/card/array-and-string/205/array-two-pointer-technique/1153/
+# Two Sum II - Input array is sorted
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        
+        # two pointer solution
+        l, r = 0, len(numbers)-1
+        
+        while l < r:
+            s = numbers[l] + numbers[r]
+            if s == target:
+                return [l+1, r+1]
+            # if sum is smaller than target
+            # l+=1 will increase sum
+            elif s < target:
+                l += 1
+            # if sum is bigger than target
+            # r-=1 will decrease sum
+            
+            else:
+                r -= 1
+            
+# https://leetcode.com/explore/learn/card/array-and-string/205/array-two-pointer-technique/1157/
+# Two-pointer Technique - Scenario II
+# Sometimes, we can use two pointers with different steps to solve problems.
+
+# https://leetcode.com/explore/learn/card/array-and-string/205/array-two-pointer-technique/1299/
+# Minimum Size Subarray Sum (Medium)
+
+# Try bruteforce again
